785b43ac3db7f0aae56e89836f254ec1726ad9bd
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-09-28 17:14:15 +0200

    final version

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -25,30 +25,43 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setupView()
         mixColors()
+        setValue()
     }
-
+    
+    // MARK: IBAction
+    @IBAction func sliderAction(_ sender: UISlider) {
+        mixColors()
+        switch sender {
+        case redSlider:
+            redLabelScore.text = string(from: sender)
+        case greenSlider:
+            greenLabelScore.text = string(from: sender)
+        default:
+            blueLabelScore.text = string(from: sender)
+        }
+    }
+    
+    // MARK: Private Methods
     private func setupView() {
         viewRGB.layer.cornerRadius = viewRGB.frame.height / 5
-
     }
     
     private func mixColors() {
-        viewRGB.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSLider.value), alpha: 1)
+        viewRGB.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSLider.value), alpha: 1
+        )
     }
     
-    // MARK: IBActions
-    @IBAction func redSliderAction() {
-        redLabelScore.text = (round(redSlider.value * 100) / 100).formatted()
-        mixColors()
-    }
-    @IBAction func greenSliderAction() {
-        greenLabelScore.text = (round(greenSlider.value * 100) / 100).formatted()
-        mixColors()
-    }
-    @IBAction func blueSliderAction() {
-        blueLabelScore.text = (round(blueSLider.value * 100) / 100).formatted()
-        mixColors()
+    private func setValue() {
+        redLabelScore.text = string(from: redSlider)
+        greenLabelScore.text = string(from: greenSlider)
+        blueLabelScore.text = string(from: blueSLider)
     }
     
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
 
