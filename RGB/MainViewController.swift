0cb4d2d110ba7ed5c9f322f5edc1a0f6ec168330
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-10-12 13:57:48 +0200

    update UI

---
+++ RGB/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  RGB
+//
+//  Created by Roman on 12.10.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
